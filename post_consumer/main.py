ea7e1233a226df79bd1f5302c76763e6519a3457
Misak0o <arnaud.baillif@proton.me>
2025-12-09 16:47:30 +0100

    Fixing consumer issues where datas where not formatted in JSON for BigQuery

--- post_consumer/main.py
+++ post_consumer/main.py
@@ -14,24 +14,25 @@ log = logging.getLogger(__name__)
 
 TOPIC = "posts"  # Name of the Kafka topic
 SCHEMA = [  # Define the schema for the table
-    bigquery.SchemaField('id', 'STRING', mode='REQUIRED'),
-    bigquery.SchemaField('post_type_id', 'STRING', mode='REQUIRED'),
-    bigquery.SchemaField('accepted_answer_id', 'STRING', mode='NULLABLE'),
-    bigquery.SchemaField('creation_date', 'TIMESTAMP', mode='REQUIRED'),
-    bigquery.SchemaField('score', 'INTEGER', mode='REQUIRED'),
-    bigquery.SchemaField('view_count', 'INTEGER', mode='NULLABLE'),
-    bigquery.SchemaField('body', 'STRING', mode='REQUIRED'),
-    bigquery.SchemaField('owner_user_id', 'STRING', mode='NULLABLE'),
-    bigquery.SchemaField('last_editor_user_id', 'STRING', mode='NULLABLE'),
-    bigquery.SchemaField('last_edit_date', 'TIMESTAMP', mode='NULLABLE'),
-    bigquery.SchemaField('last_activity_date', 'TIMESTAMP', mode='NULLABLE'),
-    bigquery.SchemaField('title', 'STRING', mode='NULLABLE'),
-    bigquery.SchemaField('tags', 'STRING', mode='NULLABLE'),
-    bigquery.SchemaField('answer_count', 'INTEGER', mode='NULLABLE'),
-    bigquery.SchemaField('comment_count', 'INTEGER', mode='REQUIRED'),
-    bigquery.SchemaField('content_license', 'STRING', mode='REQUIRED'),
-    bigquery.SchemaField('parent_id', 'STRING', mode='NULLABLE')
+    bigquery.SchemaField("id", "STRING", mode="REQUIRED"),
+    bigquery.SchemaField("post_type_id", "STRING", mode="REQUIRED"),
+    bigquery.SchemaField("accepted_answer_id", "STRING", mode="NULLABLE"),
+    bigquery.SchemaField("creation_date", "TIMESTAMP", mode="REQUIRED"),
+    bigquery.SchemaField("score", "INTEGER", mode="REQUIRED"),
+    bigquery.SchemaField("view_count", "INTEGER", mode="NULLABLE"),
+    bigquery.SchemaField("body", "STRING", mode="REQUIRED"),
+    bigquery.SchemaField("owner_user_id", "STRING", mode="NULLABLE"),
+    bigquery.SchemaField("last_editor_user_id", "STRING", mode="NULLABLE"),
+    bigquery.SchemaField("last_edit_date", "TIMESTAMP", mode="NULLABLE"),
+    bigquery.SchemaField("last_activity_date", "TIMESTAMP", mode="NULLABLE"),
+    bigquery.SchemaField("title", "STRING", mode="NULLABLE"),
+    bigquery.SchemaField("tags", "STRING", mode="NULLABLE"),
+    bigquery.SchemaField("answer_count", "INTEGER", mode="NULLABLE"),
+    bigquery.SchemaField("comment_count", "INTEGER", mode="REQUIRED"),
+    bigquery.SchemaField("content_license", "STRING", mode="REQUIRED"),
+    bigquery.SchemaField("parent_id", "STRING", mode="NULLABLE"),
 ]
+
 
 def create_dataset_if_not_exists(client, dataset_id, project_id):
     dataset_ref = bigquery.DatasetReference(project_id, dataset_id)
@@ -43,31 +44,37 @@ def create_dataset_if_not_exists(client, dataset_id, project_id):
         dataset = bigquery.Dataset(dataset_ref)
         dataset.location = "US"  # Adjust location as needed
         dataset.default_table_expiration_ms = (
-                3600000 * 24
+            3600000 * 24
         )  # Example: 24 hours, adjust as needed
         client.create_dataset(dataset)  # Make an API request.
         log.info(f"Created dataset {dataset_id}.")
 
+
 def transform_key(key):
     # Remove '@' and convert to snake_case
-    key = key.replace('@', '')
-    key = re.sub(r'(?<!^)(?=[A-Z])', '_', key).lower()
+    key = key.replace("@", "")
+    key = re.sub(r"(?<!^)(?=[A-Z])", "_", key).lower()
     return key
+
 
 def filter_post(post, allowed_columns):
     return {k: v for k, v in post.items() if k in allowed_columns}
+
 
 def transform_and_filter_post(post, allowed_columns):
     transformed_post = {transform_key(k): v for k, v in post.items()}
     filtered_post = filter_post(transformed_post, allowed_columns)
     return filtered_post
 
+
 def transform_post(post):
     return {transform_key(k): v for k, v in post.items()}
 
+
 def save_post_to_json(post, filepath):
-    with open(filepath, 'w') as json_file:
+    with open(filepath, "w") as json_file:
         json.dump(post, json_file)
+
 
 def post_bigquery(transformed_post):
     # Authenticate with Google Cloud and initialize the BigQuery client
@@ -114,26 +121,28 @@ def post_bigquery(transformed_post):
     else:
         log.info("Encountered errors while inserting rows: {}".format(job.errors))
 
+
 def main(multiple, kafka_host):
     # Kafka configuration
     bootstrap_servers = [kafka_host]
-    
+
     consumer = KafkaConsumer(
-        TOPIC,                         # topic name
+        TOPIC,  # topic name
         bootstrap_servers=bootstrap_servers,
-        auto_offset_reset='latest',       # start at earliest message
-        enable_auto_commit=True,            # auto commit offsets
-        group_id='super_mega_cool_group',       # consumer group
-        value_deserializer=lambda v: json.loads(v.decode('utf-8'))
+        auto_offset_reset="latest",  # start at earliest message
+        enable_auto_commit=True,  # auto commit offsets
+        group_id="super_mega_cool_group",  # consumer group
+        value_deserializer=lambda v: json.loads(v.decode("utf-8")),
     )
 
-    #consumer.subscribe([TOPIC]
+    # consumer.subscribe([TOPIC]
 
     try:
         # Produce and send a single message
-        #posts = consumer.poll();
+        # posts = consumer.poll();
         for message in consumer:
-            post_bigquery(message)
+            transformed_post = transform_post(message.value)
+            post_bigquery(transformed_post)
             print(f"Posting message {message}")
             if not multiple:
                 break
@@ -142,14 +151,20 @@ def main(multiple, kafka_host):
     finally:
         consumer.close()
 
+
 # Main execution
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--multiple', action='store_true', help='Send multiple messages')
-    parser.add_argument('--kafka_host', type=str, required=False, default=None, help='The Kafka host address, changing BigQuery target to Kafka')
+    parser.add_argument(
+        "--multiple", action="store_true", help="Send multiple messages"
+    )
+    parser.add_argument(
+        "--kafka_host",
+        type=str,
+        required=False,
+        default=None,
+        help="The Kafka host address, changing BigQuery target to Kafka",
+    )
     args = parser.parse_args()
 
-    main(
-        multiple=args.multiple,
-        kafka_host=args.kafka_host
-    )
+    main(multiple=args.multiple, kafka_host=args.kafka_host)
